86e01f40d9a5a283d121868ade5f699b03346e62
Dave <D-Bennett@users.noreply.github.com>
2026-06-13 19:52:47 +0100

    base symfony

---
+++ src/Controller/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MainController extends AbstractController
+{
+    #[Route('/', name: 'app_main')]
+    public function index(): Response
+    {
+        return $this->render('app/index.html.twig');
+    }
+}
